0f0650ecc22a0e8f0ef846939384d33d45f87509
Straania <s.danda222@gmail.com>
2024-10-09 13:53:11 +0000

    zmena

--- first.py
+++ first.py
@@ -1,1 +1,12 @@
-print("Hello World!")
+
+# first.py
+
+def sudy_nebo_lichy(cislo):
+    if cislo % 2 == 0:
+        print(f"Číslo {cislo} je sudé")
+    else:
+        print(f"Číslo {cislo} je liché")
+
+# Volání funkce s různými čísly
+sudy_nebo_lichy(5)
+sudy_nebo_lichy(1000000)
